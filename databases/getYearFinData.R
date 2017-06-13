7311b4fd945b2ef6b2d7494b2f32bd272a4bb7e1
Jesse Moore <jmoore1@ualberta.ca>
2017-06-13 20:44:18 +0200

    take 2

--- databases/getYearFinData.R
+++ databases/getYearFinData.R
@@ -7,7 +7,7 @@ library(data.table)
 ticker <- 'fb'
 
 # Get a list of supported tickets
-stockTickers <- as.vector(read.csv('data/tickers.csv'))
+stockTickers <- as.vector(fread('data/tickers.csv')[[1]])
 
 # Setup options for downloading files with XBRL package
 options(stringsAsFactors = FALSE,download.file.method = 'curl')
@@ -16,12 +16,23 @@ options(stringsAsFactors = FALSE,download.file.method = 'curl')
 
 getInfo <- function(ticker){
         IS <- GetIncome(ticker, 2017)[,c(1,3,5)]
-        fwrite(IS,paste('../shinyApp/data/income_statements/',ticker,'_is.csv',sep = ''))
+        fwrite(IS,paste('C:/Users/User/Desktop/AOTC-B/shinyApp/data/Yearly/income_statements/',ticker,'_is.csv',sep = ''))
         BS <- GetBalanceSheet(ticker, 2017)[,c(1,3,5)]
-        fwrite(BS,paste('../shinyApp/data/balance_sheets/',ticker,'_bs.csv',sep = ''))
+        fwrite(BS,paste('C:/Users/User/Desktop/AOTC-B/shinyApp/data/Yearly/balance_sheets/',ticker,'_bs.csv',sep = ''))
         CF <- GetCashFlow(ticker,2017)[,c(1,3,5)]
-        fwrite(CF,paste('../shinyApp/data/cash_flow/',ticker,'_cf.csv',sep = ''))
+        fwrite(CF,paste('C:/Users/User/Desktop/AOTC-B/shinyApp/data/Yearly/cash_flow/',ticker,'_cf.csv',sep = ''))
 }
 
         
+library(doParallel)  
+cl <- makeCluster(4)  
+registerDoParallel(cl)  
 
+foreach(i=1:4,.packages=c('XBRL','finreportr','data.table')) %dopar% {
+        try(getInfo(stockTickers[[i]]))
+        
+}
+
+stopCluster(cl)  
+
+        
